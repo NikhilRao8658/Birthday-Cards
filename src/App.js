13a110be54a7004c0ae474e5d0fbb9ff3ca5ac61
unknown <nrao8658@gmail.com>
2024-10-06 18:09:09 +0530

    comment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Birhtday from './Components/BirthCard';
+
+
+
+function App() {
+  return (
+    <div>
+      <Birhtday/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BirthCard.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+export default function Birthday() {
+    let [Date, setdate] = useState([]);
+
+    useEffect(() => {
+        let api = axios.get('http://localhost:9090/BirthData');
+        api.then(values => setdate(values.data));
+    }, []);
+
+    let ClearAll = () => {
+        setdate([]); 
+    };
+
+    return (
+        <>
+            <h2 id="Heading-1">{Date.length} Birthdays Today</h2>
+           
+            <div id="Birth-Cards-P">
+                <div id="Birth-Cards">
+                    {Date.map(x => {
+                        return (
+                            <div className="card" key={x.name}>
+                                <img src={x.image} alt="not found" id="imagesB" />
+                                <ruby id="ruby1">{x.name} <rt>{x.Age}</rt></ruby>
+                                <span id="dob">{x.Dob}</span>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div>
+                <button id="clear-all" onClick={ClearAll}>Clear All</button>
+            </div>
+            </div>
+        </>
+    );
+}
